6ef13ca4fed9f7635b916529b6c36cbe06c2cf50
unknown <lucasaraujo1964@gmail.com>
2023-03-25 10:44:11 -0300

    fix issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages'
-import SigninPage from './pages/signin'
+import SigninPage from './pages/SignIn'
 import SignupPage from './pages/SignUp'
 import ForgotPassword from './components/ForgotPassword'
 
